73ca1a3b849e75192c46de11b5d70c030a2aa85f
mlucasdev <lucasmsantos819@gmail.com>
2022-04-28 18:44:49 -0300

    connect database

---
+++ src/database/database.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export const connectDatabase = () => {
+  console.log("Connecting to database...");
+
+  mongoose
+    .connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connected MongoDB!"))
+    .catch((err) => console.log(`Error connecting to MongoDB ${err.message}`));
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import {} from "dotenv/config";
 import express from "express";
 import cors from "cors";
+import {connectDatabase} from "./database/database.js";
 
 const port = process.env.PORT || 3001;
 const app = express();
 
+connectDatabase();
+
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send({ message: "Hello, World!" });
-});
-
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+    console.log(`Server running on http://localhost:${port}`);
 })
