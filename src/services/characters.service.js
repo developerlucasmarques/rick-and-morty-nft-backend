175f51d157d2794ccca11cfb21a26768eeab0b83
mlucasdev <lucasmsantos819@gmail.com>
2022-04-30 19:38:37 -0300

    function name change

--- src/services/characters.service.js
+++ src/services/characters.service.js
@@ -4,13 +4,13 @@ const findAllCharactersService = () => Characters.find();
 
 const createCharacterService = (body) => Characters.create(body);
 
-const findCharacterByNameService = (name) => Characters.findOne({ name: name });
+const findByNameCharacterService = (name) => Characters.findOne({ name: name });
 
 const findByIdCharacterService = (idParams) => Characters.findById(idParams);
 
 export {
   findAllCharactersService,
   createCharacterService,
-  findCharacterByNameService,
+  findByNameCharacterService,
   findByIdCharacterService,
 };
