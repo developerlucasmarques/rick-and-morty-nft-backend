dcfdd519ffb778496943834083c34eda330a7edd
mlucasdev <lucasmsantos819@gmail.com>
2022-05-01 15:13:02 -0300

    add comission

--- src/models/Characters.js
+++ src/models/Characters.js
@@ -4,6 +4,7 @@ const CharactersSchema = new mongoose.Schema({
   name: { type: String, required: true },
   image: { type: String, required: true },
   price: { type: Number, required: true },
+  commission: {type: Number, required: true},
 });
 
 export const Characters = mongoose.model("characters", CharactersSchema);
