682ee11731488f6806278c49070efedfdd352801
mlucasdev <lucasmsantos819@gmail.com>
2022-05-02 00:27:07 -0300

    create swagger routes and json

--- src/index.js
+++ src/index.js
@@ -5,19 +5,20 @@ import { connectDatabase } from "./database/database.js";
 import { router } from "./routes/characters.routes.js";
 import { userRouter } from "./users/users.routes.js";
 import { authRouter } from "./auth/auth.routes.js";
+import { swaggerRouter } from "./swagger/swagger.routes.js"
 
 dotenv.config();
 const port = process.env.PORT || 3001;
 const app = express();
 
 connectDatabase();
-
 app.use(cors());
 app.use(express.json());
 
-app.use("/characters", router);
 app.use("/users", userRouter);
 app.use("/auth", authRouter);
+app.use("/characters", router);
+app.use("/api-docs", swaggerRouter)
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
