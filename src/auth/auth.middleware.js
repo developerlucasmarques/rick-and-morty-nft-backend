ed53a1834179e44b9d7bfb256f8da2d6ed45dca8
mlucasdev <lucasmsantos819@gmail.com>
2022-05-05 18:42:50 -0300

    verify jwt decoded

--- src/auth/auth.middleware.js
+++ src/auth/auth.middleware.js
@@ -1,6 +1,6 @@
-import dotenv from 'dotenv';
-import jwt from 'jsonwebtoken';
-import { findByIdUserService } from '../users/users.service.js';
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import { findByIdUserService } from "../users/users.service.js";
 
 dotenv.config();
 
@@ -8,23 +8,26 @@ const authLoginMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
-    return res.status(401).send({ message: 'O token não foi informado! ' });
+    return res.status(401).send({ message: "O token não foi informado! " });
   }
 
-  const parts = authHeader.split(' ');
+  const parts = authHeader.split(" ");
   if (parts.length !== 2) {
-    return res.status(401).send({ message: 'Token inválido' });
+    return res.status(401).send({ message: "Token inválido" });
   }
 
   const [scheme, token] = parts;
   if (!/^Bearer$/i.test(scheme)) {
-    return res.status(401).send({ message: 'Token mal formatado!' });
+    return res.status(401).send({ message: "Token mal formatado!" });
   }
 
   jwt.verify(token, process.env.SECRET, async (err, decoded) => {
+    if (!decoded) {
+      return res.status(400).send({ message: "Token inválido" });
+    }
     const user = await findByIdUserService(decoded.id);
     if (err || !user || !user.id) {
-      return res.status(401).send({ message: 'Token inválido!' });
+      return res.status(401).send({ message: "Token inválido!" });
     }
     req.userID = user.id;
 
@@ -39,13 +42,15 @@ const authVerifyUserAdminMiddleware = async (req, res, next) => {
     const [scheme, token] = parts;
 
     jwt.verify(token, process.env.SECRET, async (err, decoded) => {
+      if (!decoded) {
+        return res.status(400).send({ message: "Token inválido" });
+      }
       const user = await findByIdUserService(decoded.id);
       if (!user.admin) {
-        return res.status(400).send({ message: "Sem permissão!" });
+        return res.status(401).send({ message: "Sem permissão!" });
       }
       return next();
     });
-    
   } catch (err) {
     res.status(500).send({
       message: "Ops, tivemos um pequeno problema. Tente novamente mais tarde.",
@@ -54,7 +59,4 @@ const authVerifyUserAdminMiddleware = async (req, res, next) => {
   }
 };
 
-export {
-  authLoginMiddleware,
-  authVerifyUserAdminMiddleware
-}
+export { authLoginMiddleware, authVerifyUserAdminMiddleware };
