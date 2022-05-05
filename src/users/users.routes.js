09e69685362bff4ca2db1b661bee05f3b6f6dd7c
mlucasdev <lucasmsantos819@gmail.com>
2022-05-05 19:01:01 -0300

    add in route authLoginMiddleware and authVerifyUserAdminMiddleware

--- src/users/users.routes.js
+++ src/users/users.routes.js
@@ -1,31 +1,46 @@
-import express from 'express';
+import express from "express";
+import {
+  authLoginMiddleware,
+  authVerifyUserAdminMiddleware,
+} from "../auth/auth.middleware.js";
 export const userRouter = express.Router();
 import {
   createUserAdminController,
   createUserController,
   findAllUserController,
   findBydIdUserController,
-} from './users.controller.js';
+} from "./users.controller.js";
 
 import {
   checkAllFields,
   verifyExistingUserByEmail,
   verifyExistingUserById,
-} from './users.middlewares.js';
+} from "./users.middlewares.js";
 
 userRouter.post(
-  '/create',
+  "/create",
   checkAllFields,
   verifyExistingUserByEmail,
   createUserController
 );
 
 userRouter.post(
-  '/create-admin',
+  "/create-admin",
   checkAllFields,
   verifyExistingUserByEmail,
   createUserAdminController
-)
+);
 
-userRouter.get('/', findAllUserController);
-userRouter.get('/:id', verifyExistingUserById, findBydIdUserController);
+userRouter.get(
+  "/",
+  authLoginMiddleware,
+  authVerifyUserAdminMiddleware,
+  findAllUserController
+);
+userRouter.get(
+  "/:id",
+  authLoginMiddleware,
+  authVerifyUserAdminMiddleware,
+  verifyExistingUserById,
+  findBydIdUserController
+);
