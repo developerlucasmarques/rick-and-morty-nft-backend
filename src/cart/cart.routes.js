7251e33cd7cfe6b1e6ff11fa1b5e2fbc2e5d2cbc
mlucasdev <lucasmsantos819@gmail.com>
2022-05-07 18:13:29 -0300

    edit controller

--- src/cart/cart.routes.js
+++ src/cart/cart.routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { authLoginMiddleware } from '../auth/auth.middleware.js';
-import { createCartController } from './cart.controller.js';
+import { createAndAddCartController } from './cart.controller.js';
 
 export const cartRouter = express.Router();
 
-cartRouter.post('/create/:id', authLoginMiddleware, createCartController);
+cartRouter.post('/create/:id', authLoginMiddleware, createAndAddCartController);
