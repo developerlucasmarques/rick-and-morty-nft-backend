68965fe2db280af0543996e1aa14334386a73b14
mlucasdev <lucasmsantos819@gmail.com>
2022-05-06 22:32:44 -0300

    create Schema cart

---
+++ src/models/Cart.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const CartSchema = new mongoose.Schema({
+  characters: { type: Array, required: true },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users',
+    required: true,
+  },
+  finished: { type: Boolean, required: true },
+});
+
+export const Cart = mongoose.model('cart', CartSchema);
