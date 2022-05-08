dc3a91a9887f1a07d4cddaefe9d406c85a6b56cd
mlucasdev <lucasmsantos819@gmail.com>
2022-05-07 23:11:05 -0300

    chreate route delete by id and add middleware empty cart

--- src/cart/cart.routes.js
+++ src/cart/cart.routes.js
@@ -1,13 +1,32 @@
 import express from 'express';
 import { authLoginMiddleware } from '../auth/auth.middleware.js';
+import { verifyIdExistInDb } from '../characters/characters.middlewares.js';
 import {
   createAndAddCartController,
+  deleteCharacterCartController,
   findAllCartCharactersController,
 } from './cart.controller.js';
+import { emptyCartMiddleware } from './cart.middleware.js';
 
 export const cartRouter = express.Router();
 
-cartRouter.post('/create/:id', authLoginMiddleware, createAndAddCartController);
+cartRouter.post(
+  '/create/:id',
+  authLoginMiddleware,
+  verifyIdExistInDb,
+  createAndAddCartController
+);
 
-cartRouter.get('/list', authLoginMiddleware, findAllCartCharactersController);
-
+cartRouter.get(
+  '/list',
+  authLoginMiddleware,
+  emptyCartMiddleware,
+  findAllCartCharactersController
+);
+cartRouter.put(
+  '/delete/:id',
+  authLoginMiddleware,
+  verifyIdExistInDb,
+  emptyCartMiddleware,
+  deleteCharacterCartController
+);
