f7e27904f27ce15a3e0be5d5dd0b240cb4f474da
mlucasdev <lucasmsantos819@gmail.com>
2022-05-08 20:50:43 -0300

    add marketplaceRouter

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { userRouter } from './users/users.routes.js';
 import { authRouter } from './auth/auth.routes.js';
 import { swaggerRouter } from './swagger/swagger.routes.js';
 import { cartRouter } from './cart/cart.routes.js';
+import { marketplaceRouter } from './marketplace/marketplace.routes.js';
 
 dotenv.config();
 const port = process.env.PORT || 3001;
@@ -20,6 +21,7 @@ app.use('/users', userRouter);
 app.use('/auth', authRouter);
 app.use('/characters', router);
 app.use('/cart/', cartRouter);
+app.use('/marketplace/', marketplaceRouter);
 app.use('/api-docs', swaggerRouter);
 
 app.listen(port, () => {
