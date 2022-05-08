6df13d263a8d87c6b15efa6c67a7af130036740b
mlucasdev <lucasmsantos819@gmail.com>
2022-05-07 23:21:17 -0300

    edit name function

--- src/cart/cart.middleware.js
+++ src/cart/cart.middleware.js
@@ -1,13 +1,13 @@
 import { findByIdCartUserService } from './cart.service.js';
 
-const emptyCartMiddleware = async (req, res, next) => {
+const verifyEmptyCartMiddleware = async (req, res, next) => {
   try {
     const cart = await findByIdCartUserService(req.userId);
     if (!cart || cart.characters.length == 0) {
       return res.status(404).send({ message: 'Carrinho está vazio' });
     }
-    next()
+    next();
   } catch (err) {}
 };
 
-export { emptyCartMiddleware };
+export { verifyEmptyCartMiddleware };
