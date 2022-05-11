d9935f3f6b5eb592535f116f7ea83971dcd853f9
mlucasdev <lucasmsantos819@gmail.com>
2022-05-11 20:03:48 -0300

    verify Id Exist Db And Marketplace

--- src/cart/cart.middleware.js
+++ src/cart/cart.middleware.js
@@ -1,5 +1,8 @@
+import mongoose from 'mongoose';
 import { findOneCartUserService } from './cart.service.js';
 import { findByIdUserService } from '../users/users.service.js';
+import { findOneCharacterMarketplaceService } from '../marketplace/marketplace.service.js';
+import { findByIdCharacterService } from '../characters/characters.service.js';
 
 const verifyEmptyCartMiddleware = async (req, res, next) => {
   try {
@@ -7,7 +10,7 @@ const verifyEmptyCartMiddleware = async (req, res, next) => {
     if (!cart || cart.characters.length == 0) {
       return res.status(404).send({ message: 'Carrinho está vazio' });
     }
-    next();
+    return next();
   } catch (err) {
     res.status(500).send({
       message: 'Ops, tivemos um pequeno problema. Tente novamente mais tarde.',
@@ -33,4 +36,30 @@ const verifyAdminCartMiddleware = async (req, res, next) => {
   }
 };
 
-export { verifyEmptyCartMiddleware, verifyAdminCartMiddleware };
+const verifyIdExistDbAndMarketplace = async (req, res, next) => {
+  try {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      return res.status(400).send({ message: 'Id inválido.' });
+    }
+    const characterMk = await findOneCharacterMarketplaceService(req.params.id);
+    const characterPlatform = await findByIdCharacterService(req.params.id);
+    if (!characterMk || characterPlatform.acquired) {
+      return res.status(400).send({ message: 'Não está a disponível.' });
+    }
+    if (!characterMk || !characterPlatform) {
+      return res.status(400).send({ message: 'Id inválido.' });
+    }
+    return next();
+  } catch (err) {
+    res.status(500).send({
+      message: 'Ops, tivemos um pequeno problema. Tente novamente mais tarde.',
+    });
+    console.log(err.message);
+  }
+};
+
+export {
+  verifyEmptyCartMiddleware,
+  verifyAdminCartMiddleware,
+  verifyIdExistDbAndMarketplace,
+};
