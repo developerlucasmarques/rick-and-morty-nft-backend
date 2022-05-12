5f5251eab15d7cb54038193b158529314e176256
mlucasdev <lucasmsantos819@gmail.com>
2022-05-12 19:33:39 -0300

    add characters array by id

--- src/models/Cart.js
+++ src/models/Cart.js
@@ -1,9 +1,14 @@
 import mongoose from 'mongoose';
 
 const CartSchema = new mongoose.Schema({
-  characters: { type: Array, required: true },
+  characters: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'characters',
+      required: true,
+    },
+  ],
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true },
-  finished: { type: Boolean, required: true },
 });
 
 export const Cart = mongoose.model('carts', CartSchema);
